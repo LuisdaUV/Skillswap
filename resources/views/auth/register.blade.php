a3f29f111ceddc0c1e500a698a9a945441479427
LuisdaUV <luisdaunivalle@gmail.com>
2026-05-19 12:06:12 -0500

    Modificacion registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,12 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <div class="mt-4">
+            <x-input-label for="university_id" :value="__('Matrícula Universitaria')" />
+            <x-text-input id="university_id" class="block mt-1 w-full" type="text" name="university_id" :value="old('university_id')" required />
+            <x-input-error :messages="$errors->get('university_id')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
