f1743f4b6c754d0817d4f3c3dacc3d184ca2b6eb
LuisdaUV <luisdaunivalle@gmail.com>
2026-05-19 12:04:59 -0500

    INCIO PLANTILLA

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Página de inicio
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas protegidas (solo para usuarios logueados)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// ESTA LÍNEA ES LA MÁS IMPORTANTE: Importa Login, Registro, etc.
+require __DIR__.'/auth.php';
